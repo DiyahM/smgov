f36b7adc9fa91316aac624b952bd7753009ff9a2
Hadiyah Mujhid <diyahm@aol.com>
2012-05-16 18:19:43 -0700

    geolocation map added

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.configure(function(){
-  //app.use(gzippo.staticGzip(dirname +'/static', {clientMaxAge: maxAge}));
   app.use(express.static('./static'));
 });
 
 app.get('/', function(request, response){
-	response.send('Hello World!');
+	response.send('Loading...');
 });
 
 
